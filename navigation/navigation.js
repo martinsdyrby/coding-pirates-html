63be452c5b2cbba48eb1ea219bc16450aece08c4
MADY <mady@admins-MacBook-Pro.local>
2019-03-05 16:59:18 +0100

    Adding initial code

---
+++ navigation/navigation.js
@@ -0,0 +1,29 @@
+(function(){
+	var navigation = Array.prototype.slice.call(document.querySelectorAll('ul#navigation li'));
+	var sections = Array.prototype.slice.call(document.querySelectorAll('section'));
+
+	var hideAllSections = function() {
+		sections.forEach(function(section) {
+			section.classList.remove('active');
+		});
+	};
+
+	var showSection = function(index) {
+		if (index < sections.length) {
+			var section = sections[index];
+			section.classList.add('active');
+		}
+	};
+
+	navigation.forEach(function(entry, index) {
+		entry.addEventListener('click', function() {
+			hideAllSections();
+			showSection(index);
+		});
+	});
+
+	hideAllSections();
+	if (navigation.length > 0) {
+		showSection(0);	
+	}
+}());
